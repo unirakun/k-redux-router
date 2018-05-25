498e4871ee98d0771b2c44ef627299af39bce929
Fabien JUIF <fabien.juif@gmail.com>
2018-05-25 09:36:37 +0200

    :sparkles: selector / getResultParam

--- core/src/selectors.spec.js
+++ core/src/selectors.spec.js
@@ -28,16 +28,20 @@ describe('selectors', () => {
       result: {
         found: true,
         route: {
+          param: 'route-param-value',
           code: 'current-code',
           custom: 'custom-param-value',
+          alone: 'route-alone-value',
         },
         params: {
           path: {
-            code: 'path-value',
+            param: 'path-param-value',
+            common: 'path-value',
             other: 'path-other-value',
           },
           query: {
-            code: 'query-value',
+            param: 'query-param-value',
+            common: 'query-value',
             next: 'query-next-value',
           },
         },
@@ -54,10 +58,10 @@ describe('selectors', () => {
     // all the next tests are from the same selector
     const s = selectors(state => state.router)
     it('should get route code (current-code)', match(s.getCurrentCode))
-    it('should get param from path (path-value)', match(s.getParam('code')))
+    it('should get param from path (path-value)', match(s.getParam('common')))
     it('should get param from query (query-next-value)', match(s.getParam('next')))
     it('should get query param (query-next-value)', match(s.getQueryParam('next')))
-    it('should get path param (path-value)', match(s.getPathParam('code')))
+    it('should get path param (path-value)', match(s.getPathParam('common')))
     it('should get all the query params', match(s.getQueryParams))
     it('should get all the path params', match(s.getPathParams))
     it('should get all the params', match(s.getParams))
@@ -65,5 +69,6 @@ describe('selectors', () => {
     it('should get the current route definition', match(s.getCurrentRoute))
     it('should get the result', match(s.getResult))
     it('should get the route -socials-', match(s.getRoute('socials')))
+    it('should get the result route params', match(s.getResultParam('alone')))
   })
 })
