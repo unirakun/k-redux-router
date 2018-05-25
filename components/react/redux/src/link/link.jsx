f6e0825a1857cec907eec2ed72a929ce103715a2
Fabien JUIF <fabien.juif@gmail.com>
2018-05-25 10:53:43 +0200

    :sparkles: component / link / href can be a string

--- components/react/redux/src/link/link.jsx
+++ components/react/redux/src/link/link.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 // https://stackoverflow.com/questions/1714786/query-string-encoding-of-a-javascript-object
 const toQueryString = obj => Object.keys(obj).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(obj[k])}`).join('&')
 
+// TODO: factorize code with react-redux
 const Link = (props) => {
   const {
     href,
@@ -16,9 +17,12 @@ const Link = (props) => {
     children,
   } = params
 
+  if (href === undefined) return null
+
   // TODO: factorise code from middleware and from here
   let queryPart = ''
-  let processedHref = href.base
+  let processedHref = href
+  if (href.base) processedHref = href.base
   if (href.compiled) processedHref = href.compiled(params)
   if (params.query) queryPart = `?${toQueryString(params.query)}`
   processedHref = `${processedHref}${queryPart}`
@@ -37,9 +41,13 @@ const Link = (props) => {
 Link.propTypes = {
   className: PropTypes.string,
   children: PropTypes.node,
-  href: PropTypes.shape({
-    base: PropTypes.string,
-  }),
+  href: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.shape({
+      base: PropTypes.string,
+      compiled: PropTypes.func,
+    }),
+  ]),
   onClick: PropTypes.func,
 }
 
